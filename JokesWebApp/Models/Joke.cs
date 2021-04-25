b0597beab60fe782b279ca94b6abde5bbf2015d9
abc <abc@example.com>
2021-04-25 10:50:00 -0700

    Add project files.

---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string JokeQuetion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        public Joke()
+        {
+
+        }
+    }
+}
