bcef54ae38b4eb25401046edd8e69b28eb48a079
abc <abc@example.com>
2021-05-01 11:20:28 -0700

    making edits

--- JokesWebApp/Controllers/HomeController.cs
+++ JokesWebApp/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace JokesWebApp.Controllers
             return View();
         }
 
+        public IActionResult gifs()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
